a68494730242d26dee280288f2bf39cd5fd69026
KaenNietsch <kaennietsch355@gmail.com>
2026-03-17 03:53:01 +0300

    Add files via upload

---
+++ iksir_milano/script.js
@@ -0,0 +1,3 @@
+// All scripts are embedded in index.html for single-file deployment
+// This file is kept for template compatibility
+console.log('İksir Milano Güzellik Merkezi - Landing Page Loaded');
